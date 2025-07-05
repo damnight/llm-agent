358182a4cf3a634118973fabfc799eb92f87fc1d
Lucy Dust <lucrezia.d.ast@protonmail.com>
2025-07-05 23:58:07 +0200

    added get_file_info and tests

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,5 +1,6 @@
 import os
 
+MAX_CHARS = 10000
 
 def get_files_info(working_directory, directory=""):
     path = os.path.join(working_directory, directory)
@@ -18,3 +19,31 @@ def get_files_info(working_directory, directory=""):
         is_dir = os.path.isdir(os.path.join(abs_path, file))
         result += f"- {file}: file_size={file_size} bytes, is_dir={is_dir}\n"
     return result
+
+
+def get_file_content(working_directory, file_path):
+    path = os.path.join(working_directory, file_path)
+    abs_wd = os.path.abspath(working_directory)
+    abs_path = os.path.abspath(path)
+    if not abs_path.startswith(abs_wd):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    elif not os.path.isfile(abs_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    
+    try:
+        with open(abs_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+        
+            if len(file_content_string) >= 10000:
+                file_content_string += f"[...File \"{file_path}\" truncated at 10000 characters]"
+        
+            return file_content_string
+    except FileNotFoundError as e:
+        print(f"Error: FileNotFoundError: {e}")
+    except IOError as e:
+        print(f"Error: IOError: {e}")
+    except Exception as e:
+        print(f"Error: unkown error: {e}")
+
+
+
--- tests.py
+++ tests.py
@@ -1,26 +1,46 @@
 import unittest
-from functions.get_files_info import get_files_info
+from calculator.pkg import calculator
+from functions.get_files_info import get_file_content, get_files_info
 
 
 class TestFunctions(unittest.TestCase):
     def setUp(self):
         pass
     
-    def test_base_directory(self):
-        res = get_files_info("calculator", ".")
+#   def test_base_directory(self):
+#       res = get_files_info("calculator", ".")
+#       print(res)
+#
+#   def test_sub_directory(self):
+#       res = get_files_info("calculator", "pkg")
+#       print(res)
+#
+#   def test_invalid_directory(self):
+#       res = get_files_info("calculator", "/bin")
+#       print(res)
+#
+#   def test_super_directory(self):
+#       res = get_files_info("calculator", "../")
+#       print(res)
+#
+#
+#   def test_truncate(self):
+#       res = get_file_content("calculator", "lorem.txt")
+#       print(res)
+#
+
+    def test_successfully_main(self):
+        res = get_file_content("calculator", "main.py")
         print(res)
 
-    def test_sub_directory(self):
-        res = get_files_info("calculator", "pkg")
+    def test_succesfully_subdir(self):
+        res = get_file_content("calculator", "pkg/calculator.py")
         print(res)
 
-    def test_invalid_directory(self):
-        res = get_files_info("calculator", "/bin")
+    def test_invalid(self):
+        res = get_file_content("calculator", "/bin/cat")
         print(res)
 
-    def test_super_directory(self):
-        res = get_files_info("calculator", "../")
-        print(res)
 
 
 
